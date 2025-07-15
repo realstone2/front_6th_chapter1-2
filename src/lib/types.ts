12c73052d2154ebde7a2e2ba11bcda4c87c30aa8
진돌 <yeojinseok@kakao.com>
2025-07-16 00:22:38 +0900

    refactor(lib): ChildrenType 및 ElementType 타입 수정
    - ChildrenType에서 VNodeType["type"] 제거
    - ElementType에서 children 매개변수 제거
    - VNodeType 인터페이스를 타입으로 변경

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -17,18 +17,24 @@ export const HtmlElementTypes = [
 
 export type HtmlElementType = (typeof HtmlElementTypes)[number];
 
-export type ChildrenType = VNodeType | VNodeType["type"];
+export type ChildrenType = VNodeType;
 
-export type ElementType = ((props: PropsType, children: Array<ChildrenType>) => VNodeType) | HtmlElementType;
+export type ElementType = ((props: PropsType) => VNodeType) | HtmlElementType;
 export type PropsType = Record<string, any>;
 
 export type CreateVNodeArgsType = [ElementType, PropsType, ...Array<ChildrenType>];
 
-export interface VNodeType {
-  type: HtmlElementType | ((props: PropsType, children: Array<ChildrenType>) => VNodeType);
-  props: Record<string, any>; //TODO: 각 Type별로 props 타입 정의
-  children?: Array<ChildrenType>;
-}
+export type VNodeType =
+  | {
+      type: HtmlElementType | ((props: PropsType) => VNodeType);
+      props: Record<string, any>; //TODO: 각 Type별로 props 타입 정의
+      children?: Array<ChildrenType>;
+    }
+  | string
+  | number
+  | boolean
+  | null
+  | undefined;
 
 export function isVNodeType(obj: any): obj is VNodeType {
   return obj && typeof obj === "object" && "type" in obj && "props" in obj && "children" in obj;
